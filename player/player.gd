b2a08606cc7b4d39ae6565f0b2fc1ed3abe996c2
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2024-12-11 22:17:39 +0600

    camera shake

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed:float
 @export var accel:float
 
+@onready var camera:Camera2D = $Camera2D
+
 var grenade_load:PackedScene = preload("res://player/explosive/explosive.tscn")
 
 func _physics_process(delta: float) -> void:
@@ -17,7 +19,10 @@ func _physics_process(delta: float) -> void:
 		var grenade:RigidBody2D = grenade_load.instantiate()
 		grenade.position = global_position
 
-
 		var forward_force:int = 500
 		get_tree().root.call_deferred("add_child",grenade)
 		grenade.apply_central_impulse(input_direction * forward_force)
+
+
+func camera_shake():
+	$Camera2D.apply_shake()
