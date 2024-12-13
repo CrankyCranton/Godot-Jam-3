45824b2e6375ec7e7c15b2c749c3a3c00f0de187
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2024-12-12 22:58:20 -0500

    Target remainder, pause screen & time lose condition

--- scripts/targets/target.gd
+++ scripts/targets/target.gd
@@ -1,5 +1,17 @@
 class_name Target extends StaticBody2D
 
 
+signal destroyed
+
+var is_destroyed := false
+
+@onready var sprite: Sprite2D = $Sprite
+@onready var light: PointLight2D = $Light
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 func explode() -> void:
-	queue_free()
+	if not is_destroyed:
+		is_destroyed = true
+		animation_player.play(&"destroy")
+		destroyed.emit()
