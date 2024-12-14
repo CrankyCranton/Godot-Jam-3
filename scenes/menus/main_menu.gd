b5470b78999f000c7e356802f9e54442c2dc9e51
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2024-12-15 04:04:31 +0600

    pushie osh

--- scenes/menus/main_menu.gd
+++ scenes/menus/main_menu.gd
@@ -9,3 +9,6 @@ func _on_settings_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/tile_stuff/tutorial.tscn")
